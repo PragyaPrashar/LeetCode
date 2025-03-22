4499b9954e662bcac6e4251d0c3ed88ac6f6f685
Pragya <pragyaprashar490@gmail.com>
2025-03-22 11:43:43 -0400

    TwoSum

---
+++ src/main/java/TwoSum.java
@@ -0,0 +1,32 @@
+import java.util.*;
+public class TwoSum {
+  static public int[] twoSum(int[] nums, int target) {
+    Map<Integer, Integer> map=new HashMap<>();
+    int [] result=new int[2];
+    for(int i=0;i<nums.length;i++){
+      int n=target-nums[i];
+      if(map.containsKey(n)){
+        result[0]=i;
+        result[1]=map.get(n);
+        return result;
+      }
+      map.put(nums[i],i);
+    }
+    return result;
+  }
+
+  public static void main(String[] args) {
+    int [] nums={2,7,11,15};
+    int target=9;
+    int [] res=twoSum(nums, target);
+    for(int i=0;i<res.length;i++){
+      if(i==res.length-1){
+        System.out.print(res[i] );
+      }else{
+        System.out.print(res[i]+",");
+      }
+
+    }
+
+  }
+}
