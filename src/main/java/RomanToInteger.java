e1f642c3fd6cc0110e79a09c39962a592d3fd6cb
Pragya <pragyaprashar490@gmail.com>
2025-03-24 19:27:58 -0400

    New RomanToInteger

--- src/main/java/RomanToInteger.java
+++ src/main/java/RomanToInteger.java
@@ -11,15 +11,15 @@ public class RomanToInteger {
       map.put('C',100);
       map.put('D',500);
       map.put('M',1000);
-      int res=0;
+      int result=0;
       for(int i=0;i<s.length();i++){
         if(i+1<s.length()&& map.get(s.charAt(i))<map.get(s.charAt(i+1))){
-          res-=map.get(s.charAt(i));
+          result-=map.get(s.charAt(i));
         }else{
-          res+=map.get(s.charAt(i));
+          result+=map.get(s.charAt(i));
         }
       }
-      return res;
+      return result;
     }
 
   public static void main(String[] args) {
